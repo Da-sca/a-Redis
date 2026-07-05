4e9959114e33d0449860bbda170229fdd2b40107
Da-sca <mbodja8@gmail.com>
2026-07-05 10:55:21 +0000

    Collision setup

---
+++ src/collision.h
@@ -0,0 +1,11 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "hashtable.h"
+#include "value.h"
+
+Node* find_in_bucket(Node* bucket_head, const char* key);
+void insert_or_update(HashTable* ht, const char* key, Value value);
+int delete_from_bucket(HashTable* ht, const char* key);
+
+#endif
